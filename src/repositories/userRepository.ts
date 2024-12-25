ef171c0c15bea22b414d77bb525eba18491f1dc8
S0lux <loctranphuoc456@protonmail.com>
2024-12-25 15:52:41 +0700

    Added default user avatar

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -3,7 +3,9 @@ import { FriendRequestStatus, PrismaClient, UserRelation } from "@prisma/client"
 const prisma = new PrismaClient();
 
 const createUser = async (data: { username: string; hashedPassword: string; email: string }) => {
-  return await prisma.user.create({ data });
+  return await prisma.user.create({
+    data: { ...data, avatarUrl: `https://api.dicebear.com/9.x/initials/png?seed=${data.username}` },
+  });
 };
 
 const getUserByUsername = async (username: string) => {
@@ -72,7 +74,7 @@ const banUser = async (userId: string) => {
     where: { id: userId },
     data: { status: "SUSPENDED" },
   });
-}
+};
 
 export const userRepository = {
   createUser,
